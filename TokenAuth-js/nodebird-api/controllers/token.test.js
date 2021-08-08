57175c0a5f1547264f041de8ac1dfbdef1b37185
unn04012 <unn04012@naver.com>
2021-08-08 22:03:00 +0900

    indexRouter 통합테스트

---
+++ TokenAuth-js/nodebird-api/controllers/token.test.js
@@ -0,0 +1,8 @@
+jeset.mock('../models/domain');
+import { describe } from 'yargs';
+import Domain from '../models/domain';
+import User from '../models/user';
+
+describe('create Token', () => {
+    test()
+})
