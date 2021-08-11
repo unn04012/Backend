8f9b5646c9a132e1a189c550bc0dbe47e8164ef2
unn04012 <unn04012@naver.com>
2021-08-11 22:07:58 +0900

    웹 소캣 설치

---
+++ gif-chat/routes/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+export default class Index{
+    path = '/';        
+    router = express.Router();
+    constructor(){        
+        this.router.get('/', this.index);        
+    }
+    index = async(req, res, next) => {
+        try{              
+            res.render('index', {head : 'express template with es6'})
+        }catch(err){
+            console.error(err);
+            next(err);
+        }
+    }    
+}
---
+++ gif-chat/server.js
@@ -0,0 +1,92 @@
+import express from 'express';
+import path from 'path';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
+import WebSocket from 'ws';
+
+import Index from './routes/Index';
+
+import App from './app';
+
+
+
+dotenv.config();
+
+/**
+ * DEFINE MODELS
+ * @OBJECT
+ * name : modelName;
+ * value : modelObject
+ * 
+ * const models = [
+    {name : 'User',   value : User},
+    {name : 'Domain', value : Domain},
+ ];
+  
+const model = new modelIndex(models);
+  
+
+
+
+ model.db.sequelize.sync({ force: false })
+      .then(() => {
+        console.log('Database connection successful');
+      })
+      .catch((err) => {
+        console.error(err);
+ });
+ */
+
+ 
+
+/**
+ * DEFINE ROUTES
+ * @OBJECT
+ */
+const routes = [    
+    new Index(),    
+];
+
+/**
+ * DEFINE MIDDLEWARES
+ */
+
+const middlewares = [   
+    morgan('dev'),
+    express.static(path.join(__dirname, 'public')),
+    express.json(),
+    express.urlencoded({extended: false}),        
+    cookieParser(process.env.COOKIE_SECRET),
+    session({
+      resave: false,
+      saveUninitialized: false,
+      secret: process.env.COOKIE_SECRET,
+      cookie: {
+          httpOnly: true,
+          secure: false,
+      }
+    }),   
+];
+
+/**
+ * DEFINE SETTINGS FOR express().set
+ */
+
+const settings = [
+  {key : 'port', value : process.env.PORT || 8002},
+  {key : 'view engine', value : 'html'},
+]
+
+const appConfig = {
+    routes : routes,
+    middlewares : middlewares,
+    settings : settings,
+    port : process.env.PORT || 3000,
+};
+
+// CREATE SERVER
+const server = new App(appConfig).listen();
+WebSocket(server);
+
