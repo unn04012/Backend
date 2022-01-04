55f3c57c13174a13a2ec694ae46fdb2e12cd5614
unn04012 <unn04012@naver.com>
2022-01-04 17:18:22 +0900

    [백준][우선순위 큐] 최소힙 최대힙

---
+++ algorithms/baekjun/priority_queue/min_heap.js
@@ -0,0 +1,76 @@
+class MinHeap {
+  constructor() {
+    this.heap = [null];
+  }
+  push(value) {
+    this.heap.push(value);
+    let curIndex = this.heap.length - 1; // 현재 추가된 index
+    let parentIndex = Math.floor(curIndex / 2); // 추가된 인덱스의 부모 인덱스
+
+    while (parentIndex !== 0 && this.heap[parentIndex] > value) {
+      const temp = this.heap[parentIndex];
+      this.heap[parentIndex] = value;
+      this.heap[curIndex] = temp;
+
+      curIndex = parentIndex;
+      parentIndex = Math.floor(curIndex / 2);
+    }
+  }
+
+  pop() {
+    const returnValue = this.heap[1];
+    if (returnValue === undefined) return 0;
+    if (this.heap.length === 2) return this.heap.pop();
+    this.heap[1] = this.heap.pop();
+
+    let curIndex = 1;
+    let leftIndex = 2;
+    let rightIndex = 3;
+
+    while (
+      this.heap[curIndex] > this.heap[leftIndex] ||
+      this.heap[curIndex] > this.heap[rightIndex]
+    ) {
+      if (this.heap[leftIndex] > this.heap[rightIndex]) {
+        const temp = this.heap[curIndex];
+        this.heap[curIndex] = this.heap[rightIndex];
+        this.heap[rightIndex] = temp;
+        curIndex = rightIndex;
+      } else {
+        const temp = this.heap[curIndex];
+        this.heap[curIndex] = this.heap[leftIndex];
+        this.heap[leftIndex] = temp;
+        curIndex = leftIndex;
+      }
+      leftIndex = curIndex * 2;
+      rightIndex = curIndex * 2 + 1;
+    }
+    return returnValue;
+  }
+}
+
+let input = require("fs")
+  .readFileSync("../problem.txt")
+  .toString()
+  .trim()
+  .split("\n");
+console.log(
+  solution(
+    Number(input[0]),
+    input.slice(1, input.length).map((e) => Number(e))
+  )
+);
+
+function solution(count, numbers) {
+  let answer = [];
+  let minHeap = new MinHeap();
+  for (const number of numbers) {
+    if (number === 0) {
+      answer.push(minHeap.pop());
+      continue;
+    }
+    minHeap.push(number);
+  }
+
+  return answer.join("\n");
+}
